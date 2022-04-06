dc23e911af093e5e21ea7de171d44ca9b66ba945
Cankat <cankatabaci95@gmail.com>
2022-04-07 00:22:06 +0300

    add zodiacs

---
+++ src/constants/Zodiac.ts
@@ -0,0 +1,14 @@
+export class Zodiac{
+    public static readonly ARIES = "Aries";
+    public static readonly TAURUS = "Taurus";
+    public static readonly GEMINI = "Gemini";
+    public static readonly CANCER = "Cancer";
+    public static readonly LEO = "Leo";
+    public static readonly VIRGO = "Virgo";
+    public static readonly LIBRA = "Libra";
+    public static readonly SCORPIO = "Scorpio";
+    public static readonly SAGITTARIUS = "Sagittarius";
+    public static readonly CAPRICORNUS = "Capricornus";
+    public static readonly AQUARIUS = "Aquarius";
+    public static readonly PISCES = "Pisces";
+}
