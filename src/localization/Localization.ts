503a0ee7d6fff97a2c1f6dbee8188f732f382262
Cankat <cankatabaci95@gmail.com>
2022-04-14 22:50:01 +0300

    fix local jsons

--- src/localization/Localization.ts
+++ src/localization/Localization.ts
@@ -1,5 +1,9 @@
 import { ErrorMessages } from "../constants/ErrorMessages";
 import { LangCodes } from "../constants/LangCodes";
+import enJson from "./en.json";
+import trJson from "./tr.json";
+import frJson from "./fr.json";
+import itJson from "./it.json";
 
 export class Localization{
     public getObjectByLang(lang:string):any{
